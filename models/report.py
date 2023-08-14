061e163e031e71e5d8a964381133ba48b2b3ca9d
tinhnguyen229 <thaibinh2292001@gmail.com>
2023-08-14 12:22:18 +0700

    update code

--- models/report.py
+++ models/report.py
@@ -8,16 +8,17 @@ class Report(models.Model):
     total_expense = fields.Float(string='Tổng chi', compute='_compute_total_expense') # , compute='_compute_total_expense'
     balance = fields.Float(string='Quỹ còn lại', compute='_compute_balance') # , compute='_compute_balance'
 
-
     def _compute_total_income(self):
-        incomes = self.env['income'].search([])
-        income_list = incomes.mapped('payment')
-        self.total_income = sum(income_list)
+        for r in self:
+            incomes = r.env['income'].search([])
+            income_list = incomes.mapped('payment')
+            r.total_income = sum(income_list)
 
     def _compute_total_expense(self):
-        expenses = self.env['expense'].search([])
-        expense_list = expenses.mapped('expense_amount')
-        self.total_expense = sum(expense_list)
+        for r in self:
+            expenses = r.env['expense'].search([])
+            expense_list = expenses.mapped('expense_amount')
+            r.total_expense = sum(expense_list)
 
     def _compute_balance(self):
         for r in self:
